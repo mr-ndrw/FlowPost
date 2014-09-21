cad1ce525b1ac1fa97d7f2e81c3fc4419f983fa3
mr-ndrw <andrew.torski@gmail.com>
2014-09-21 20:58:09 +0200

    Correct addition of Topic class.

---
+++ FlowPost/Logic/Logic.Entities/Topic.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace en.AndrewTorski.FlowPost.Logic.Entities
+{
+    /// <summary>
+    /// Topical Post - a post with no parent post.
+    /// </summary>
+    public class Topic : Post
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Hidden status.
+        /// </summary>
+        public bool IsHidden { get; set; }
+
+        /// <summary>
+        ///     Closed status.
+        /// </summary>
+        public bool IsClosed { get; set; }
+
+        /// <summary>
+        ///     Sticky status.
+        /// </summary>
+        public bool IsSticky { get; set; }
+
+        /// <summary>
+        ///     Extraordinary status.
+        /// </summary>
+        public bool IsExtraordinary { get; set; }
+
+        /// <summary>
+        ///     Number of views this Topic has gotten.
+        /// </summary>
+        public int Views { get; set; } 
+
+        #endregion
+
+        #region NavigationProperties
+
+        /// <summary>
+        ///     Collection of posts which are replies to this Topic.
+        /// </summary>
+        public virtual ICollection<Post> Posts { get; set; }
+
+        #endregion
+    }
+}
