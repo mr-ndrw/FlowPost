16edb7535a6eb4ec4dde1301113c8f29b62c4983
mr-ndrw <andrew.torski@gmail.com>
2014-10-10 22:17:52 +0200

    Just a simple modification

--- FlowPost/Persistance/Persistance.Core/Specialized/CategoryRepository.cs
+++ FlowPost/Persistance/Persistance.Core/Specialized/CategoryRepository.cs
@@ -1,12 +1,14 @@
 ﻿using en.AndrewTorski.FlowPost.Logic.Entities;
+using en.AndrewTorski.FlowPost.Persistance.Core.Abstract;
 using en.AndrewTorski.FlowPost.Persistance.Interfaces;
+using en.AndrewTorski.FlowPost.Persistance.Interfaces.Specialized;
 
 namespace en.AndrewTorski.FlowPost.Persistance.Core.Specialized
 {
 	/// <summary>
 	///		Specialized interace defining behaviour specific to Category entity.
 	/// </summary>
-	public interface ICategoryRepository : IRepository<Category>
+	public class CategoryRepository : ICategoryRepository, RepositoryBase<Category>
 	{
 	}
 }
